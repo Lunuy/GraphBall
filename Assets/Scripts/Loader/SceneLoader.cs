b3d7c886cc39d79f26a2910a19cac831989f8f66
noname <48761044+noname0310@users.noreply.github.com>
2022-06-16 15:02:18 +0900

    add loader

---
+++ Assets/Scripts/Loader/SceneLoader.cs
@@ -0,0 +1,15 @@
+#nullable enable
+using UnityEngine;
+
+namespace Assets.Scripts.Loader
+{
+    // ReSharper disable once UnusedMember.Global
+    public class SceneLoader : MonoBehaviour
+    {
+        // ReSharper disable once UnusedMember.Local
+        private void Awake()
+        {
+
+        }
+    }
+}
