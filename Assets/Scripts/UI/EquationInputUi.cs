29927b00f77eb16614dae3da7e6c16d6dac7a894
noname <48761044+noname0310@users.noreply.github.com>
2022-06-18 14:52:14 +0900

    commit

---
+++ Assets/Scripts/UI/EquationInputUi.cs
@@ -0,0 +1,15 @@
+﻿#nullable enable
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    // ReSharper disable once UnusedMember.Global
+    public class EquationInputUi : MonoBehaviour
+    {
+        public GameObject InputPanel = null!;
+        public TMP_InputField EquationInputField = null!;
+        public TMP_Text LeftCharacterText = null!;
+        public TMP_Text ParseErrorText = null!;
+    }
+}
