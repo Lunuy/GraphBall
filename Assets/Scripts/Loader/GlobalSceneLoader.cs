6c5ad88fed96b84e5cd60d069861fb8fd92eeafe
noname <48761044+noname0310@users.noreply.github.com>
2022-06-19 11:49:55 +0900

    Update GlobalSceneLoader.cs

--- Assets/Scripts/Loader/GlobalSceneLoader.cs
+++ Assets/Scripts/Loader/GlobalSceneLoader.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Assets.Scripts.Utility;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Object = UnityEngine.Object;
