5f4cad020d9efa322608bd00b8046cc1ca68a4e8
noname <48761044+noname0310@users.noreply.github.com>
2022-06-21 01:22:19 +0900

    shadow path builder

---
+++ Assets/Editor/InGame/ShadowCasterCircleShapeMakerEditor.cs
@@ -0,0 +1,57 @@
+﻿#nullable enable
+using Assets.Scripts.InGame;
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Editor.InGame
+{
+    [CustomEditor(typeof(ShadowCasterCircleShapeMaker))]
+    [CanEditMultipleObjects]
+    // ReSharper disable once UnusedMember.Global
+    public class ShadowCasterCircleShapeMakerEditor : UnityEditor.Editor
+    {
+        private ShadowCasterCircleShapeMaker? _shadowCasterCircleShapeMaker;
+
+        // ReSharper disable once UnusedMember.Local
+        private void OnEnable()
+        {
+            _shadowCasterCircleShapeMaker = (ShadowCasterCircleShapeMaker) target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            {
+                EditorGUI.BeginChangeCheck();
+                var sides = EditorGUILayout.IntField("sides", _shadowCasterCircleShapeMaker!.Sides);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(_shadowCasterCircleShapeMaker, "change sides");
+                    _shadowCasterCircleShapeMaker.Sides = sides;
+                    EditorUtility.SetDirty(_shadowCasterCircleShapeMaker);
+                }
+            }
+
+            {
+                EditorGUI.BeginChangeCheck();
+                var radius = EditorGUILayout.FloatField("radius", _shadowCasterCircleShapeMaker!.Radius);
+                if (EditorGUI.EndChangeCheck())
+                {
+                    Undo.RecordObject(_shadowCasterCircleShapeMaker, "change radius");
+                    _shadowCasterCircleShapeMaker.Radius = radius;
+                    EditorUtility.SetDirty(_shadowCasterCircleShapeMaker);
+                }
+            }
+
+
+            var buildShadowPath = GUILayout.Button("build shadow path");
+
+            if (!buildShadowPath) return;
+            if (_shadowCasterCircleShapeMaker != null)
+            {
+                _shadowCasterCircleShapeMaker.BuildPath(
+                    _shadowCasterCircleShapeMaker.Sides,
+                    _shadowCasterCircleShapeMaker.Radius);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/InGame/ShadowCasterCircleShapeMaker.cs
@@ -0,0 +1,33 @@
+﻿#nullable enable
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace Assets.Scripts.InGame
+{
+    [RequireComponent(typeof(ShadowCaster2D))]
+    // ReSharper disable once UnusedMember.Global
+    public class ShadowCasterCircleShapeMaker : MonoBehaviour
+    {
+        public int Sides = 15;
+        public float Radius = 1f;
+
+        public void BuildPath(int sides, float radius)
+        {
+            var points = new Vector3[sides];
+            var angle = Mathf.Deg2Rad * 360 / sides;
+            for (var i = 0; i < sides; i++)
+            {
+                points[i] = new Vector2(
+                    radius * Mathf.Cos(angle * i + Mathf.PI / 2),
+                    radius * Mathf.Sin(angle * i + Mathf.PI / 2)
+                );
+            }
+
+            var field = typeof(ShadowCaster2D).GetField(
+                "m_ShapePath",
+                BindingFlags.NonPublic | BindingFlags.GetField | BindingFlags.Instance);
+            field?.SetValue(GetComponent<ShadowCaster2D>(), points);
+        }
+    }
+}
